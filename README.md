2b1cc574652a53a630a8bfb7e8b9c6c584fbf4b6
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2016-10-12 01:32:44 -0700

    Works :D

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Only tested on CentOS 7.2.
 
-Fork of Yuko's OKC Code :3 Just a fun little thing and a chance to practice C / threads.
+Fork of Yuko's OKC Code :3 Just a fun little thing and a chance to practice C. Tried to get fancy with threads, but found it wasn't necessary to get the same point across.
 
 We start full of life, and home and work drain us over time. But we also do things that make us feel alive, scattered randomly throughout life.
--- main.c
+++ main.c
@@ -1,66 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <stdbool.h>
 
-#define NUM_THREADS 4
+#define MAX_HP 20
+#define MAX_BANDWIDTH 10
+#define UNFUN_FACTOR 5
 
-// http://randu.org/tutorials/threads/
-
-/* create thread argument struct for thr_func() */
-typedef struct _thread_data_t {
-  int tid;
-  char *stuff[];
-} thread_data_t;
-
-void *live(void *arg) {
-  thread_data_t *data = (thread_data_t *)arg;
-  sleep((int)(rand()%2));
-  printf("printing alive\n");
-  printf("I feel so alive %s!\n",
-    (char*)(data->stuff)[(int)(rand()%5)]);
-  pthread_exit(NULL);
-}
+int bandwidth, i, HP;
+bool living;
 
 int main(int argc, char **argv) {
-  pthread_t thr[NUM_THREADS];
-  int bandwidth, rc;
-  /* create a thread_data_t argument array */
-  thread_data_t thr_data[NUM_THREADS];
-
-  int i;
-  int HP = 100;
-  bool living = true;
-  const char *todo[2] = {"work","home"};
-  const char *beingAlive[5] = {
+  HP = MAX_HP;
+  bandwidth = MAX_BANDWIDTH;
+  living = true;
+  char *todo[2] = {"work","home"};
+  char *beingAlive[5] = {
     "exploring"
     ,"learning"
-    ,"being silly"
     ,"dancing"
     ,"singing"
+    ,"being generally silly"
   };
   srand(time(NULL));
 
-  for (i = 0; i < NUM_THREADS; ++i) {
-    thr_data[i].tid = bandwidth;
-    if ((rc = pthread_create(&thr[i], NULL, live, &thr_data[i]))) {
-      printf("thread error\n");
-      fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
-      return EXIT_FAILURE;
+  for (i = 0; i < MAX_BANDWIDTH+MAX_HP-1; ++i) {
+    // randomly select live or exist
+    if((int)(rand()%UNFUN_FACTOR) == 0  && bandwidth > 0) {
+      printf("I feel so alive %s!\n", beingAlive[(int)(rand()%5)]);
+      bandwidth--;
+    } else {
+      printf("%s\n", todo[(int)(rand()%2)]);
+      HP--;
     }
   }
-  while(HP > 0){
-    printf("printing work/home\n");
-    printf("%s\n", todo[(int)(rand()%2)]);
-    HP--;
-    if(HP <= 0){
-      for (i = 0; i < NUM_THREADS; ++i) {
-        pthread_join(thr[i], NULL);
-      } // can't die until you're done living
-      living = false;
-      break;
-    }
-  }
-  printf("Well, good game.");
+  printf("Time to just enjoy being alive for a bit\n");
+  HP--;
+
+  living = false; // can't die until you're done living
+
+  printf("Well, good game.\n");
   return EXIT_SUCCESS;
 }
